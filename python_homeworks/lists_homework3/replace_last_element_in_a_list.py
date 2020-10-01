033e6ebf4624e52d237840aad2352c606502565f
liviusighiartau <sighiartauliviu@gmail.com>
2020-10-01 15:26:16 +0300

    Create a function to replace the last element in the list

---
+++ python_homeworks/lists_homework3/replace_last_element_in_a_list.py
@@ -0,0 +1,7 @@
+def replace_last_element_in_list(my_list: list, replacer='last'):
+    my_list_updated = [
+        replacer
+        if index == len(my_list) - 1
+        else element
+        for index, element in enumerate(my_list)]
+    return my_list_updated
