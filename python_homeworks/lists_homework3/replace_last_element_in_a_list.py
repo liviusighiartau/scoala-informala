eb187a7c39af1bd5dbf7851d4d974af3739fbfa0
liviusighiartau <sighiartauliviu@gmail.com>
2020-10-04 15:27:56 +0300

    Coding style and naming changes

--- python_homeworks/lists_homework3/replace_last_element_in_a_list.py
+++ python_homeworks/lists_homework3/replace_last_element_in_a_list.py
@@ -3,5 +3,6 @@ def replace_last_element_in_list(my_list: list, replacer='last'):
         replacer
         if index == len(my_list) - 1
         else element
-        for index, element in enumerate(my_list)]
+        for index, element in enumerate(my_list)
+    ]
     return my_list_updated
--- python_homeworks/lists_homework3/return_strings_from_list.py
+++ python_homeworks/lists_homework3/return_strings_from_list.py
@@ -1,9 +1,10 @@
 def return_string_from_list(my_list: list):
-    my_list_updated = [
+    my_strings_list = [
         element
         for element in my_list
-        if type(element) is str]
-    return my_list_updated
+        if type(element) is str
+    ]
+    return my_strings_list
 
 
 # my_list = ['I', 'am', 'a', 1, 'list', 'of', 5, 'strings']
